0ce2c64f5aa814aab9ccdf04f7c7706205d62bf2
Fefo <federico.barberi@gmail.com>
2022-05-09 09:13:43 +0200

    Initial commit

---
+++ src/main/java/com/example/weatherapp/Controllers/WeatherController.java
@@ -0,0 +1,29 @@
+package com.example.weatherapp.Controllers;
+
+import com.WeatherService;
+import com.example.weatherapp.Models.WeatherInput;
+import com.example.weatherapp.Models.WeatherOutputModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/weather")
+public class WeatherController {
+
+    @Autowired
+    private WeatherService weatherService;
+
+    @GetMapping("/current")
+    public WeatherOutputModel currentWeather(@RequestParam("city") String city) {
+        WeatherInput weatherInput = weatherService.getWeatherForCity(city);
+        WeatherOutputModel weatherOutputModel = new WeatherOutputModel(city, weatherInput.getMain().getTemp(), weatherInput.getMain().getFeelsLike());
+        return weatherOutputModel;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/example/weatherapp/Models/WeatherData.java
@@ -0,0 +1,13 @@
+package com.example.weatherapp.Models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+@Data
+public class WeatherData {
+
+    @JsonProperty("temp")
+    private String temp;
+    @JsonProperty("feels_like")
+    private String feelsLike;
+}
